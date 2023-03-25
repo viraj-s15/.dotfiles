829826fdcf31def31cc57c3f46b20a0e7a3ece0a
viraj-s15 <veer15102003@gmail.com>
2023-03-25 13:26:17 +0530

    Updated Config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,16 +1,28 @@
-local wezterm = require('wezterm')
-local ssh_domains = require('extra.ssh') 
-return {
-	-- color scheme
-	color_scheme = "Banana Blueberry",
-	--window opacity reduced
-	window_background_opacity = .85,
-	-- make sure you use a font you have installed
-	font = wezterm.font 'NotoSansMono Nerd Font',
-	font_size = 10.5,
-	-- scroll bar
-	enable_scroll_bar = true,
-	-- Custom Key Bindings
-	-- disable_default_key_bindings = true,
-	ssh_domains = ssh_domains,
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {
+-- window_background_image = "/home/veer/Pictures/wp5.jpg",
+font_size = 15.0,
 }
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+-- if wezterm.config_builder then
+--   config = wezterm.config_builder()
+-- end
+--
+-- -- This is where you actually apply your config choices
+-- local mux = wezterm.mux
+--
+-- wezterm.on('gui-startup', function(cmd)
+--   local tab, pane, window = mux.spawn_window(cmd or {})
+--   window:gui_window():maximize()
+-- end)
+
+
+-- For example, changing the color scheme:
+
+-- and finally, return the configuration to wezterm
+return config
