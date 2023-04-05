e2c3064ddec511594520c826b57aa0ce75398b80
viraj-s15 <veer15102003@gmail.com>
2023-04-05 13:48:05 +0530

    Updated Config

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "oxocarbon",
+  colorscheme = "carbonfox",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
