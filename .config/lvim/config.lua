2f77772c1a6892ec3bd5ac6eb4544c2e26a6cc84
Viraj Shah <79002760+viraj-s15@users.noreply.github.com>
2023-12-02 19:32:23 +0530

    Updated neovim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -53,7 +53,7 @@ lvim.plugins = {
         },
 
         disable = {
-          colored_cursor = false, -- Disable the colored cursor
+          colored_cursor = true, -- Disable the colored cursor
           borders = false,        -- Disable borders between verticaly split windows
           background = false,     -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
           term_colors = false,    -- Prevent the theme from setting terminal colors
@@ -62,7 +62,7 @@ lvim.plugins = {
 
         high_visibility = {
           lighter = false, -- Enable higher contrast text for lighter style
-          darker = false   -- Enable higher contrast text for darker style
+          darker = true -- Enable higher contrast text for darker style
         },
 
         lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
@@ -76,7 +76,6 @@ lvim.plugins = {
     end,
   },
   { "nyoom-engineering/oxocarbon.nvim" },
-  { "catppuccin/nvim",                 name = "catppuccin" },
   { "EdenEast/nightfox.nvim" },
   { 'ellisonleao/carbon-now.nvim' },
   { 'shaunsingh/moonlight.nvim' },
@@ -91,12 +90,128 @@ lvim.plugins = {
   {
     "folke/tokyonight.nvim",
   },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
 }
 
+require("catppuccin").setup({
+  flavour = "mocha", -- latte, frappe, macchiato, mocha
+  background = {     -- :h background
+    light = "latte",
+    dark = "mocha",
+  },
+  transparent_background = true, -- disables setting the background color.
+  show_end_of_buffer = false,    -- shows the '~' characters after the end of buffers
+  term_colors = false,           -- sets terminal colors (e.g. `g:terminal_color_0`)
+  dim_inactive = {
+    enabled = false,             -- dims the background color of inactive window
+    shade = "dark",
+    percentage = 0.15,           -- percentage of the shade to apply to the inactive window
+  },
+  styles = {                     -- Handles the styles of general hi groups (see `:h highlight-args`):
+    comments = { "italic" },     -- Change the style of comments
+    conditionals = { "italic" },
+    loops = { "bold" },
+    functions = { "bold", "italic" },
+    keywords = { "bold" },
+    strings = {},
+    variables = {},
+    numbers = {},
+    booleans = { "bold" },
+    properties = {},
+    types = { "italic" },
+    operators = {},
+  },
+  color_overrides = {
+    all = {
+      base = '#11111b',
+    },
+  },
+  custom_highlights = {},
+  integrations = {
+    cmp = true,
+    gitsigns = true,
+    nvimtree = true,
+    treesitter = true,
+    notify = false,
+    mini = {
+      enabled = true,
+      indentscope_color = "",
+    },
+    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+  },
+})
+
+require("tokyonight").setup({
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  light_style = "day", -- The theme is used when the background is set to light
+  transparent = false, -- Enable this to disable setting the background color
+  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+  styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    comments = { italic = true },
+    keywords = {bold=true, italic = true },
+    functions = {bold = true, italic=true},
+    variables = {italic=true},
+    -- Background styles. Can be "dark", "transparent" or "normal"
+    sidebars = "dark", -- style for sidebars, see below
+    floats = "dark", -- style for floating windows
+  },
+  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+  dim_inactive = false, -- dims inactive windows
+  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+  --- You can override specific color groups to use other groups or a hex color
+  --- function will be called with a ColorScheme table
+  ---@param colors ColorScheme
+  on_colors = function(colors) end,
+
+  --- You can override specific highlights to use other groups or a hex color
+  --- function will be called with a Highlights and ColorScheme table
+  ---@param highlights Highlights
+  ---@param colors ColorScheme
+on_highlights = function(hl, c)
+    local prompt = "#2d3149"
+    hl.TelescopeNormal = {
+      bg = c.bg_dark,
+      fg = c.fg_dark,
+    }
+    hl.TelescopeBorder = {
+      bg = c.bg_dark,
+      fg = c.bg_dark,
+    }
+    hl.TelescopePromptNormal = {
+      bg = prompt,
+    }
+    hl.TelescopePromptBorder = {
+      bg = prompt,
+      fg = prompt,
+    }
+    hl.TelescopePromptTitle = {
+      bg = prompt,
+      fg = prompt,
+    }
+    hl.TelescopePreviewTitle = {
+      bg = c.bg_dark,
+      fg = c.bg_dark,
+    }
+    hl.TelescopeResultsTitle = {
+      bg = c.bg_dark,
+      fg = c.bg_dark,
+    }
+  end,
+})
+
 -- COLORSCHEME
-lvim.colorscheme = "oxocarbon"
+-- lvim.colorscheme = "catppuccin-mocha"
+lvim.colorscheme = "tokyonight"
+-- lvim.colorscheme = "oxocarbon"
 vim.api.nvim_set_option("clipboard", "unnamed")
-vim.g.material_style = "darker"
+-- vim.g.material_style = "darker"
 vim.g.neovide_scale_factor = 1.05
 vim.o.guifont = "JetBrains Mono Medium:h14" -- text below applies for VimScript
 -- vim.o.guifont = "DejaVu Sans Mono:h14" -- text below applies for VimScript
