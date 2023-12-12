f7e97cda0edea2fc6c9a445ee1a7d918ef5bb397
Viraj Shah <79002760+viraj-s15@users.noreply.github.com>
2023-12-13 00:48:28 +0530

    Updated neovim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,5 +1,54 @@
 -- PLUGINS
 lvim.plugins = {
+{
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+require('copilot').setup({
+  panel = {
+    enabled = true,
+    auto_refresh = true,
+    keymap = {
+      jump_prev = "[[",
+      jump_next = "]]",
+      accept = "<CR>",
+      refresh = "gr",
+      open = "<M-CR>"
+    },
+    layout = {
+      position = "bottom", -- | top | left | right
+      ratio = 0.4
+    },
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    debounce = 75,
+    keymap = {
+      accept = "<M-l>",
+      accept_word = false,
+      accept_line = false,
+      next = "<M-]>",
+      prev = "<M-[>",
+      dismiss = "<C-]>",
+    },
+  },
+  filetypes = {
+    yaml = false,
+    markdown = false,
+    help = false,
+    gitcommit = false,
+    gitrebase = false,
+    hgcommit = false,
+    svn = false,
+    cvs = false,
+    ["."] = false,
+  },
+  copilot_node_command = 'node', -- Node.js version must be > 18.x
+  server_opts_overrides = {},
+})  end,
+},
   {
     'nvim-telescope/telescope.nvim',
     version = '0.1.4',
@@ -248,7 +297,7 @@ on_highlights = function(hl, c)
 
 -- COLORSCHEME
 lvim.colorscheme = "catppuccin-mocha"
--- lvim.colorscheme = "tokyodark"
+-- lvim.colorscheme = "tokyonight"
 -- lvim.colorscheme = "oxocarbon"
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
@@ -312,6 +361,8 @@ formatters.setup {
     filetypes = { "typescript", "typescriptreact" },
   },
 }
+
+-- require("copilot").setup(options)
 
 -- Create a new split window at the top
 vim.cmd('split')
